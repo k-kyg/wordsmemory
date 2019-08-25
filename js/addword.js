042dc6fb8d73f31286bf3d9006f312c02f979e59
k-kyg <kygkd38@gmail.com>
2019-08-25 22:28:27 +0900

    修正

--- js/addword.js
+++ js/addword.js
@@ -2,11 +2,11 @@ const words = Object.entries(JSON.parse(localStorage.getItem('words')));
 const add = () => {
 	try{
 		if (!wordkey.value || !wordmean.value) throw new Error();
+		words.push([wordkey.value, wordmean.value]);
+		localStorage.setItem('words', JSON.stringify(Object.fromEntries(words)));
+		alert(`追加しました：\n単語: ${wordkey.value}\n意味: ${wordmean.value}`);
 	}catch{
 		alert("テキストボックスが空です\n単語を入力してください");
 	}
-	words.push([wordkey.value, wordmean.value]);
-	localStorage.setItem('words', JSON.stringify(Object.fromEntries(words)));
-	alert(`追加しました：\n単語: ${wordkey.value}\n意味: ${wordmean.value}`);
 	wordkey.value = wordmean.value = "";
 }
