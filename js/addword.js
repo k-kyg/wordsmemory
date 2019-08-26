dc7323e719ebf3bcbd66c81f50a51876d72a740c
k-kyg <kygkd38@gmail.com>
2019-08-26 17:26:47 +0900

    エラー文言修正

--- js/addword.js
+++ js/addword.js
@@ -1,7 +1,7 @@
 const words = Object.entries(JSON.parse(localStorage.getItem('words')));
 const add = () => {
 	try{
-		if (!wordkey.value || !wordmean.value) throw new Error();
+		if (!wordkey.value || !wordmean.value) throw new Error("Null words content");
 		words.push([wordkey.value, wordmean.value]);
 		localStorage.setItem('words', JSON.stringify(Object.fromEntries(words)));
 		alert(`追加しました：\n単語: ${wordkey.value}\n意味: ${wordmean.value}`);
